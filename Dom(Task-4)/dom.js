9792cd73a0d155f0f5408a097d0fb4c63c96504f
Nisha-Rohilla <rohilla.nisha15@gmail.com>
2023-06-13 18:36:21 +0530

    Dom task-4 completed

--- Dom(Task-4)/dom.js
+++ Dom(Task-4)/dom.js
@@ -1,7 +1,10 @@
-let items = document.getElementsByClassName("list-group-item");
+let items = document.getElementsByClassName('list-group-item')
 
-items[2].style.backgroundColor = "green";
+// the 3 rd element in the list have green background color
+items[2].style.backgroundColor = "green"
 
-for (let i = 0; i < items.length; i++) {
+//  all the elements in the list have bold color font
+
+for(let i=0; i<items.length; i++){
   items[i].style.fontWeight = "bold";
 }
