70b55c257bb59b15c5138d12055d7c016dd6f333
Nisha-Rohilla <rohilla.nisha15@gmail.com>
2023-06-13 23:00:20 +0530

    Dom task-6 completed

--- Dom(Task-6)/script.js
+++ Dom(Task-6)/script.js
@@ -1,9 +1,22 @@
 //  QUERYSELECTOR //
-let secondItem = document.querySelector(".list-group-item:nth-child(2)");
-secondItem.style.color = "green";
 
-let items = document.querySelectorAll("li:nth-child(odd)");
+// the 2nd item have green background color
+let secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor = "green"
 
-for (let i = 0; i < items.length; i++) {
-  items[i].style.backgroundColor = "green";
+// the 3rd item invisible
+let thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.display = "none"
+
+// QUERYSELECTORALL
+
+let secondli = document.querySelectorAll('li')
+// change the font color to green for 2nd item in the item list
+secondli[1].style.color = "green";
+
+let oddElement = document.querySelectorAll('li:nth-child(odd)');
+
+// Odd Elements background color(Green)
+for(let i=0; i<oddElement.length; i++){
+  oddElement[i].style.backgroundColor = 'green';
 }
